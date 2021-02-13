c02ecaadf2ade1d9e1caf6394a71a88bc6bc8075
rhargreaves <robert@roberthargreaves.net>
2021-02-13 13:22:18 +0000

    Read keys from env

--- buyer/app.js
+++ buyer/app.js
@@ -1,15 +1,15 @@
 'use strict';
 const {CoinbasePro} = require('coinbase-pro-node');
 const auth = {
-    apiKey: '',
-    apiSecret: '',
-    passphrase: '',
-    useSandbox: true
+  apiKey: process.env.BUYER_CB_API_KEY,
+  apiSecret: process.env.BUYER_CB_API_SECRET,
+  passphrase: process.env.BUYER_CB_PASSPHRASE,
+  useSandbox: process.env.BUYER_CB_USE_SANDBOX
 };
 const client = new CoinbasePro(auth);
 
 client.rest.account.listAccounts().then(accounts => {
-    const message = `You can trade "${accounts.length}" different pairs.`;
-    console.log(message);
+  const message = `You can trade "${accounts.length}" different pairs.`;
+  console.log(message);
 });
 
