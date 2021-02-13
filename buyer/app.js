6d41a2c01c9e7c0450aa6608486533bdfc2d7fed
rhargreaves <robert@roberthargreaves.net>
2021-02-13 14:53:52 +0000

    Remove candles code for now

--- buyer/app.js
+++ buyer/app.js
@@ -9,16 +9,6 @@ process.on('unhandledRejection', (err) => {
 
 (async () => {
   const client = coinbaseClient();
-  const candles = await client.rest.product.getCandles('BTC-USD', {
-    end: '2020-04-11T10:00:00.000Z',
-    granularity: CandleGranularity.ONE_HOUR,
-    start: '2020-04-11T08:00:00.000Z',
-  });
-
-  console.info(
-      `Received "${candles.length}" candles to represent 3 hours (08 - 11 AM).`,
-      candles);
-
   accounts = await client.rest.account.listAccounts();
   console.log(`You can trade "${accounts.length}" different pairs.`);
 })().catch(console.error);
