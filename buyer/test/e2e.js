2ed1d3798b5602085ffab0c6f01633fb0b1f3443
rhargreaves <robert@roberthargreaves.net>
2021-02-13 23:00:49 +0000

    Refactor

--- buyer/test/e2e.js
+++ buyer/test/e2e.js
@@ -3,20 +3,20 @@ const {assert} = require('chai');
 const should = require('chai').should();
 const {coinbaseClient} = require('../client.js');
 
+async function topUpAccount(client) {
+  const accounts = await client.getCoinbaseAccounts();
+  const bank = accounts.find(a => a.currency === 'GBP');
+  const deposit = await client.depositCoinbase(
+      {amount: 250, currency: 'GBP', coinbase_account_id: bank.id});
+  deposit.id.should.be.ok;
+  console.log(`Deposited ${deposit.amount} ${deposit.currency}.`);
+};
+
 describe('when purchasing coins', () => {
   const client = coinbaseClient();
 
   before(async () => {
-    const methods = await client.getPaymentMethods();
-    const achPaymentMethodId =
-        methods.find(o => o.type == 'ach_bank_account').id;
-    const deposit = await client.deposit({
-      amount: 10000,
-      currency: 'USD',
-      payment_method_id: achPaymentMethodId
-    });
-    deposit.id.should.be.ok;
-    console.log(`Deposited ${deposit.amount} ${deposit.currency}.`);
+    topUpAccount(client);
   });
 
   it('places an order sucessfully', async () => {
