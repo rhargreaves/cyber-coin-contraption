e7612d646b7199c76696615d4343539f520b4ba9
rhargreaves <robert@roberthargreaves.net>
2021-02-13 14:15:08 +0000

    Dockerise

--- buyer/app.js
+++ buyer/app.js
@@ -1,5 +1,5 @@
 'use strict';
-const {CoinbasePro} = require('coinbase-pro-node');
+const {CoinbasePro, CandleGranularity} = require('coinbase-pro-node');
 const auth = {
   apiKey: process.env.BUYER_CB_API_KEY,
   apiSecret: process.env.BUYER_CB_API_SECRET,
@@ -16,7 +16,17 @@ process.on('unhandledRejection', (err) => {
   const client = new CoinbasePro(auth);
   console.log(`Using API key ${auth.apiKey} with sandbox: ${auth.useSandbox}.`);
 
+  const candles = await client.rest.product.getCandles('BTC-USD', {
+    end: '2020-04-11T10:00:00.000Z',
+    granularity: CandleGranularity.ONE_HOUR,
+    start: '2020-04-11T08:00:00.000Z',
+  });
+
+  console.info(
+      `Received "${candles.length}" candles to represent 3 hours (08 - 11 AM).`,
+      candles);
+
+
   accounts = await client.rest.account.listAccounts();
-  const message = `You can trade "${accounts.length}" different pairs.`;
-  console.log(message);
-})();
+  console.log(`You can trade "${accounts.length}" different pairs.`);
+})().catch(console.error);
