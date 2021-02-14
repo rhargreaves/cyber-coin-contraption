4da7800fc6a0ed7b25f9fecfa37f629d61c95b5e
rhargreaves <robert@roberthargreaves.net>
2021-02-14 19:30:03 +0000

    Return order ID from lambda

--- buyer/index.js
+++ buyer/index.js
@@ -5,7 +5,7 @@ const {defaultClient} = require('./client');
 module.exports.handler = async (event, context) => {
   const client = defaultClient();
   const order = await buy(client);
-  console.log(order);
-  return order;
+  console.log(`Order ID = ${order.id}`);
+  return order.id;
 };
 
